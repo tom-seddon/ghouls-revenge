tom-seddon/ghouls-revenge

commit 2ccce51a7b4ce1d8fc77e6cf47f50273556294ef

Run GPSETUP earlier in the process.
Includes the Tube check, which should really be done before fiddling
with ROMSEL. (Should probably be part of gparty_loader anyway? I don't
remember why it's arranged this way...)

src/gparty_loader.bas
@@ -1,4 +1,5 @@
 MODE7:HIMEM=&3C00
+*RUN GPSETUP
 {?cheat}W%=INKEY-90
 *FX119
 CODESIZE=150:DIMCODE CODESIZE,BUF 512
@@ -28,7 +29,6 @@ PRINT"Using sideways RAM bank ";BEST
 *LOAD GPARTY0 3C00
 X%=BEST:CALLWRITE
 {:load}
-*RUN GPSETUP
 ?{&gp_rom_bank}=BEST
 PAGE=&1200:CHAIN"GPARTY"
 DEFFNCANWRITE(X%):=((USRCANWRITE)AND&01000000)=0

commit 24afe76719458cf81b7ba64f8990bfbc8426f7c6

Fix cold boot launch.
Fix dumb bug introduced in 2ccce51a7b4ce1d8fc77e6cf47f50273556294ef.

src/gparty_loader.bas
@@ -1,5 +1,7 @@
-MODE7:HIMEM=&3C00
-*RUN GPSETUP
+MODE7:CLOSE#0
+A%=&EA:X%=0:Y%=&FF:IF((USR&FFF4)AND&FF00)DIV256<>0:PRINT"Not compatible with a second processor":END
+A%=114:X%=1:Y%=0:CALL&FFF4{#disable shadow RAM
+HIMEM=&3C00
 {?cheat}W%=INKEY-90
 *FX119
 CODESIZE=150:DIMCODE CODESIZE,BUF 512
@@ -29,6 +31,7 @@ PRINT"Using sideways RAM bank ";BEST
 *LOAD GPARTY0 3C00
 X%=BEST:CALLWRITE
 {:load}
+*RUN GPSETUP
 ?{&gp_rom_bank}=BEST
 PAGE=&1200:CHAIN"GPARTY"
 DEFFNCANWRITE(X%):=((USRCANWRITE)AND&01000000)=0

src/gparty_setup.s65
@@ -5,28 +5,11 @@ party=true
                 .dsection code
                 .cerror *>$7c00
 
+                ; This stuff could really do with moving into GPMC or
+                ; GPLOADER.
+
                 .section code
 main:
-                ; disable shadow RAM
-                lda #114
-                ldx #1
-                ldy #0
-                jsr osbyte
-
-                ;
-                lda #$ea
-                ldx #$00
-                ldy #$ff
-                jsr osbyte
-                cpx #$00
-                beq copro_ok
-
-                brk
-                .byte 255
-                .text 'Not compatible with a second processor'
-                brk
-
-copro_ok:
                 jsr define_udgs
 
                 ldx #<load_gpmc